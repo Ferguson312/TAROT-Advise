8ed4173c844b9ce3db0a2f3effd75aa5e43c1071
holodnoo <evaperepecaeva@gmail.com>
2024-05-29 23:09:41 +0500

    taro

--- js/answer.js
+++ js/answer.js
@@ -1,25 +1,32 @@
+function validateQuestion() {
+    var questionInput = document.querySelector('.answer_question').value.trim();
+    if (questionInput === "") {
+        alert("Пожалуйста, введите ваш вопрос.");
+        return false;
+    }
+    return true;
+}
+
+function showRandomAnswer(event) {
+    event.preventDefault();
+
+    // Проверяем введенный вопрос
+    if (!validateQuestion()) {
+        return;
+    }
+
+    var randomNumber = Math.floor(Math.random() * 2) + 1;
+    var cardImg = document.querySelector('.card2_img');
+    var cardDesc = document.querySelector('.answer__info-desc');
+
+    switch (randomNumber) {
+        case 1:
+            cardImg.src = "/img2/YES.PNG";
+            break;
+        case 2:
+            cardImg.src = '/img2/NO.PNG';
+            break;
+    }
+}
+
 document.querySelector('.answer__btn').addEventListener('click', showRandomAnswer);
-
-        function showRandomAnswer(event) {
-            event.preventDefault();
-
-
-            var randomNumber = Math.floor(Math.random() * 2) + 1;
-            var cardImg = document.querySelector('.card2_img');
-            var cardDesc = document.querySelector('.answer__info-desc');
-
-            switch (randomNumber) {
-                case 1:
-                    cardImg.src = "/img2/YES.PNG";
-                    
-                    break;
-                case 2:
-                    cardImg.src = '/img2/NO.PNG';
-                
-                    break;
-
-
-
-
-            }
-        }
