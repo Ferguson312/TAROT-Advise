e7b1d79b07b8364d193a45f93af142f60f909194
Туренков Владислав <137440160+Ferguson312@users.noreply.github.com>
2024-06-06 12:23:38 +0500

    Js

--- js/pyramid.js
+++ js/pyramid.js
@@ -1,21 +1,28 @@
-document.querySelector('.answer__btn').addEventListener('click', showRandomCard);
+document.querySelector('.pyramid__btn').addEventListener('click', showRandomTarotCard);
 
-        function showRandomCard(event) {
+        function showRandomTarotCard(event) {
             event.preventDefault();
 
-            var randomNumber = Math.floor(Math.random() * 2) + 1;
+
+            var randomNumber = Math.floor(Math.random() * 1) + 1;
             var cardImg = document.querySelector('.card3_img');
-           
-            switch (randomNumber) 
-            {
-                case 1:
-                    cardImg.src = "/img2/02.jpg";
-                    break;
-               
-                case 2:
-                    cardImg.src = "/img2/02.jpg";
-                    break;
-            
-            
-            }
+            var cardImg2 = document.querySelector('.card4_img');
+            var cardImg3 = document.querySelector('.card5_img');
+            var cardImg4 = document.querySelector('.card6_img');
+            var cardDesc = document.querySelector('.pyramid__info-desc');
+
+            var myPix = new Array("img2/02.jpg","img2/03.jpg","img2/04.jpg","img2/05.jpg","img2/06.jpg","img2/07.jpg","img2/08.jpg","img2/09.jpg");
+            var randomNum = Math.floor(Math.random() * myPix.length);
+            cardImg.src = myPix[randomNum];
+            var randomNum = Math.floor(Math.random() * myPix.length);
+            cardImg2.src = myPix[randomNum];
+            var randomNum = Math.floor(Math.random() * myPix.length);
+            cardImg3.src = myPix[randomNum];
+            var randomNum = Math.floor(Math.random() * myPix.length);
+            cardImg4.src = myPix[randomNum];
+        
+
+
+
+
         }
